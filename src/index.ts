ecf7eb7a20beb6eacedf2a40800ab96f5f8f57ed
haoolii <unnhao@gmail.com>
2024-12-23 15:50:46 +0800

    feat: add vercel file

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Hello Express & Vercel !!");
 });
 
