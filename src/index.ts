b9cc1effcd8aedf38960dc4f6711fc2f5ec55a2d
haoolii <unnhao@gmail.com>
2024-12-23 16:09:47 +0800

    feat: use nodemon

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello Express & Vercel !!");
 });
 
